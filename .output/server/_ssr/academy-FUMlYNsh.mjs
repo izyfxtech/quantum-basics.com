390ff857c340f27880a497a51a7ae6d7c62640b1
Israel Eberendu <ieberendu.official@gmail.com>
2026-08-29 09:27:52 +0100

    Delete .output directory

--- .output/server/_ssr/academy-FUMlYNsh.mjs
+++
@@ -1,4 +0,0 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/academy-FUMlYNsh.js
-var academy_default = "/assets/academy-C6Ks0Fq5.jpg";
-//#endregion
-export { academy_default as t };
--- .output/server/_ssr/smart-field-C5zrQ7Lx.mjs
+++
@@ -1,4 +0,0 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/smart-field-C5zrQ7Lx.js
-var smart_field_default = "/assets/smart-field-CRYSBtUM.jpg";
-//#endregion
-export { smart_field_default as t };
